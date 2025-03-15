0f122127c58f05773604934cb666c72a8e8e62ed
ozakaushal <kaushaloza48@gmail.com>
2025-03-15 18:00:22 +0530

    prepared about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Kaushal's blog
+          </h1>
+          <div className="text-m text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+              animi doloribus, quasi asperiores nulla quidem, qui iure obcaecati
+              eaque voluptatum delectus dignissimos facere maiores libero
+              repellendus expedita quae vitae incidunt. Fugit commodi placeat
+              recusandae odio.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+              animi doloribus, quasi asperiores nulla quidem, qui iure obcaecati
+              eaque voluptatum delectus dignissimos facere maiores libero
+              repellendus expedita quae vitae incidunt. Fugit commodi placeat
+              recusandae odio.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+              animi doloribus, quasi asperiores nulla quidem, qui iure obcaecati
+              eaque voluptatum delectus dignissimos facere maiores libero
+              repellendus expedita quae vitae incidunt. Fugit commodi placeat
+              recusandae odio.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
